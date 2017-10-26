1df9c89b311e27b3292a5f54cd1b81b43cada44e
Syméon Carle <symeon.carle@telecomnancy.eu>
2017-10-26 12:07:51 +0200

    première ébauches lecture

--- tesh/main.c
+++ tesh/main.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#define TAILLE_TAMPON_LECTURE 64
+
 
 int main(int argc, char **argv){
 
-//Lecture
+
+    int fini = 0;
+
+    while (!fini) {
+
+        //Lecture
+        char* commande = lecture();
+
+        //Analyse
 
 
-//Analyse
+        //Exécution
 
+    }
 
-//Exécution
+}
+
+char* lecture() {
+
+    int taille_actuelle = TAILLE_TAMPON_LECTURE;
+    int position = 0;
+
+    char* commande;
+    commande = (char*) malloc(sizeof(char)*taille_actuelle);
+    
+    if (commande == NULL) {
+        print("Erreur lors de l'allocation du tampon de lecture\n");
+        return NULL;
+    }
+
+    char c = getchar();
+    while (c != EOF) {
+        if (position < taille_actuelle) {
+            commande[position] = c;
+        }
+    }
+
 }

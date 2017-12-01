19fcebd72302464ed267a4cf9b45c7369fc93087
Najib Aghenda <najib.aghenda@telecomnancy.eu>
2017-12-01 16:57:06 +0100

    Ajout de la sortie sur erreur

--- src/main.c
+++ src/main.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "lecture.h"
 #include "execution.h"
 #include "analyse.h"
+#include "errno.h"
 
 #define TAILLE_TAMPON_LECTURE 64
 
 
-int main(int argc, char **argv){
+int main(int argc, char* argv[]){
 
 	char hostname[128];
 	char pwd[1024];
     int fini = 0;
+
+    int option_e = 0;
+    int option_r = 0;
+
+    int valeur_retour = 0;
+
+    if (argc > 2) {
+        errno = 7;
+        perror("Error ");
+        exit(7);
+    }
+
+    if (strcmp(argv[1],"-e")) {
+        option_e = 1;
+    }
 
     while (!fini) {
 
@@ -23,6 +40,8 @@ int main(int argc, char **argv){
     	getcwd(pwd, 1024);
     	pwd[1023] = '\0';
     	printf("%s@%s:%s$ ",getenv("USER"),hostname,pwd);
+
+
         //Lecture
         char* commande = lecture();
 
@@ -30,7 +49,13 @@ int main(int argc, char **argv){
         char** parsed = analyse(commande);
 
         //Exécution
-        execution(parsed);
+        valeur_retour = execution(parsed);
+
+        if (option_e == 1) {
+            if (valeur_retour != 0) {
+                return 0;
+            }
+        }
 
     }
 }
